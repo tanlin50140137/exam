5b5b300de56720ec22ecbdd4c1179e804e65ff13
TanLin <39691304+tanlin50140137@users.noreply.github.com>
2018-07-21 18:17:48 +0800

    exam
    exam

--- system/config/config.php
+++ system/config/config.php
@@ -2,6 +2,6 @@
 define("SERVER", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "root");
-define("BASENAME", "htx_baidu");
+define("BASENAME", "htx_exam");
 define("BAS", "mysql");
 define("PRE", "htx_");
