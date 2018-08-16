21d927028fdde0eeb6e5fe4f08fb2122d83cd619
TanLin <39691304+tanlin50140137@users.noreply.github.com>
2018-08-16 15:33:04 +0800

    exam
    exam

--- inset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-chmod 0777 system && chmod 0777 system/.dvsd.php && chmod 0777 system/config && chmod 0777 .external && chmod 0777 .settings/.org.nums && chmod 0777 .settings/.org.shu && chmod 0777 subject/version.txt && echo "Exam Install OK";
